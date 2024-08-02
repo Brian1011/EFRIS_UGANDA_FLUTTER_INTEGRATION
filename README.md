c33b39447b8725d76dc025eff266c2b4b5a9751c
Brian Mutinda <mutindabrian26@gmail.com>
2024-08-02 10:30:41 +0300

    initialize project

---
+++ README.md
@@ -0,0 +1,13 @@
+# EFRIS FLUTTER INTEGRATION GUIDE
+This document provides a guide on how to integrate Efris Android SDK into a Flutter project.
+By the end of this guide, you will be able to:
+- Integrate EFRIS Android SDK into a Flutter project
+- Use EFRIS Android SDK to generate invoice number, verification code and QR code.
+- Handle the response from EFRIS Android SDK
+
+## Prerequisites
+- Flutter SDK
+- Android Studio
+- Efris license key
+
+## Installation
---
+++ lib/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EFRIS Demo App'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+
+          },
+          child: const Text('Get invoice'),
+        ),
+      ),
+    );
+  }
+}
